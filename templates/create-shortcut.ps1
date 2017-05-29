1472e44370650785841e3377122144a496d2eaf3
Dennis Ploeger <dennis.ploeger@kps.com>
2017-05-29 16:57:20 +0200

    Missing corrections

--- templates/create-shortcut.ps1
+++ templates/create-shortcut.ps1
@@ -1,6 +1,6 @@
 $shell = New-Object -COMObject WScript.Shell;
-$shortcut = $shell.CreateShortcut("<%= @shortcut_path %>");
-$shortcut.TargetPath = "<%= @agent_dir_win %>\bin\agent.bat";
+$shortcut = $shell.CreateShortcut("<%= scope['::teamcity:shortcut_path'] %>");
+$shortcut.TargetPath = "<%= scope['::teamcity:agent_dir_win'] %>\bin\agent.bat";
 $shortcut.Arguments = "start";
 $shortcut.Description = "Standalone TeamCity agent";
 $shortcut.Save();
--- templates/extract-agent-archive.ps1
+++ templates/extract-agent-archive.ps1
@@ -1,4 +1,4 @@
 $shell = New-Object -COMObject Shell.Application;
-$zipfile = $shell.NameSpace("<%= @temp_dir_win %>\<%= @archive_name %>");
-foreach($item in $zipfile.Items()){$shell.NameSpace("<%= @agent_dir_win %>").CopyHere($item, "20")};
+$zipfile = $shell.NameSpace("<%= scope['::teamcity::temp_dir_win'] %>\<%= @archive_name %>");
+foreach($item in $zipfile.Items()){$shell.NameSpace("<%= scope['::teamcity::agent_dir_win'] %>").CopyHere($item, "20")};
 
